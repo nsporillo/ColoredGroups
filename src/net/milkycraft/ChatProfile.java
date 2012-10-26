97f3187456fcaf6e5b398971a108ffc311dd8009
Nick Porillo <milkywayz@mail.com>
2012-10-26 16:11:27 -0400

    Create format to be cached

--- src/net/milkycraft/ChatProfile.java
+++ src/net/milkycraft/ChatProfile.java
@@ -8,30 +8,46 @@ public class ChatProfile {
 	private String prefix;
 	private String suffix;
 	private String muffix;
+	private String format;
 
-	public ChatProfile(String g, String prefix2, String suffix2, String muffix2) {
+	public ChatProfile(String g, String prefix2, String suffix2,
+			String muffix2, String format2) {
 		this.g = g;
 		this.prefix = prefix2;
 		prefix = prefix.replace("&", "");
 		this.suffix = suffix2;
 		suffix = suffix.replace("&", "");
-		this.muffix = muffix2;	
+		this.muffix = muffix2;
 		muffix = muffix.replace("&", "");
+		this.format = format2;
+		format();
 	}
 
 	public final ChatColor getPrefix() {
-		return ChatColor.getByChar(prefix);
+		return ChatColor.getByChar(this.prefix);
 	}
 
 	public final ChatColor getSuffix() {
-		return ChatColor.getByChar(suffix);
+		return ChatColor.getByChar(this.suffix);
 	}
-	
+
 	public final ChatColor getMuffix() {
 		return ChatColor.getByChar(this.muffix);
 	}
+
 	public final String getGroup() {
 		return g;
 	}
 
+	public final String getFormat() {
+		return format;
+	}
+
+	private void format() {
+		format = format
+				.replace(format.substring(0, 1) + "g",
+						getPrefix() + format.substring(0, 1) + getGroup())
+				.replace("p", getSuffix() + "%s")
+				.replace("m", getMuffix() + "%s").replace("null", "§f");
+	}
 }
