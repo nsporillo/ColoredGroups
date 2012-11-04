13856c273bc32bac9fefffc7f275963b85a04795
Nick Porillo <milkywayz@mail.com>
2012-11-04 01:32:38 -0400

    Added the source for an example addon

---
+++ src/addons/BetterChatAddon/src/BetterChatAddon.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import net.milkycraft.ChatHandler;
+import net.milkycraft.ChatProfile;
+import net.milkycraft.ColoredGroups;
+import net.milkycraft.addons.Addon;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.Plugin;
+
+public class BetterChatAddon implements Addon, Listener {
+
+	private ColoredGroups cg = null;
+	private String version = "1.0";
+	private String name = "BetterChatAddon";
+	private String description = "Adds world to chat";
+	private List<String> authors = new ArrayList<String>();
+	
+	@Override
+	public void enable() {
+		authors.add("milkywayz");
+		try {
+			/*Hook into ColoredGroups*/
+			Plugin c = Bukkit.getPluginManager().getPlugin("ColoredGroups");
+			if (c != null) {
+				cg = (ColoredGroups) c;
+			} else {
+				System.out.println("Could not hook into ColoredGroups");
+			}
+			/*Disable the default chat listener*/
+			AsyncPlayerChatEvent.getHandlerList().unregister(new ChatHandler(cg));
+			/*Enable the new chat listener */
+			cg.getServer().getPluginManager().registerEvents(this, cg);
+		} catch (Exception exe) {
+			System.out.println("Enabling of " + this.getClass().getSimpleName()
+					+ " has failed due to " + exe.getLocalizedMessage());
+		}
+	}
+	
+	@Override
+	public void disable() {
+		/*Register our default chat listener again*/
+		cg.getServer().getPluginManager().registerEvents(new ChatHandler(cg), cg);
+		/*Disable the one we made in this addon*/
+		AsyncPlayerChatEvent.getHandlerList().unregister(this);
+	}
+
+	@Override
+	public List<String> getAuthors() {
+		return authors;
+	}
+
+	@Override
+	public String getDescription() {
+		return description;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getVersion() {
+		return version;
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onChat(AsyncPlayerChatEvent e) {
+		final String group = cg.getGroup(e.getPlayer());
+		String world = e.getPlayer().getWorld().getName()
+				.replace("world_the_end", "The_End")
+				.replace("world_nether", "Nether").replace("w", "W");
+		for (ChatProfile c : cg.getChatProfiles()) {
+			if (c.getGroup().equalsIgnoreCase(group)) {
+				e.setFormat(ChatColor.YELLOW + "[" + world + "]"
+						+ ChatColor.RESET + c.getFormat());
+				break;
+			}
+		}
+		if (e.getFormat() == "<%1$s> %2$s") {
+			e.setFormat(ChatColor.GRAY + "[" + group + "]" + "%s:"
+					+ ChatColor.WHITE + " %s");
+		}
+	}
+}
