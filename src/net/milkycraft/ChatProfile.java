08ad19c533bd9fd9f0e5762b35b60c09baa8e5cc
Nick Porillo <milkywayz@mail.com>
2012-11-17 14:11:12 -0500

    Alot of new changes

--- src/net/milkycraft/ChatProfile.java
+++ src/net/milkycraft/ChatProfile.java
@@ -4,67 +4,112 @@ import org.bukkit.ChatColor;
 
 public final class ChatProfile {
 
-	private String g;
-	private String sg;
+	private String group;
+	private String showngroup;
 	private String prefix;
 	private String suffix;
 	private String muffix;
 	private String format;
 
-	protected ChatProfile(final String g, final String sg,
-			final String prefix2, final String suffix2, final String muffix2,
-			final String format2) {
-		this.g = g;
-		this.sg = sg;
-		this.prefix = prefix2;
-		prefix = prefix.replace("&", "");
-		this.suffix = suffix2;
-		suffix = suffix.replace("&", "");
-		this.muffix = muffix2;
-		muffix = muffix.replace("&", "");
-		this.format = format2;
-		format();
+	protected ChatProfile(String g, String sg, String prefix2, String suffix2,
+			String muffix2, String format2) {
+		this.group = g;
+		this.setShownGroup(sg);
+		this.prefix = prefix2.replace("&", "");
+		this.suffix = suffix2.replace("&", "");
+		this.muffix = muffix2.replace("&", "");
+		this.format(format2);
 	}
 
-	public final ChatColor getPrefix() {
+	public ChatColor getPrefix() {
 		return ChatColor.getByChar(this.prefix);
 	}
 
-	public final ChatColor getSuffix() {
+	public ChatColor getSuffix() {
 		return ChatColor.getByChar(this.suffix);
 	}
 
-	public final ChatColor getMuffix() {
+	public ChatColor getMuffix() {
 		return ChatColor.getByChar(this.muffix);
 	}
 
 	public String getGroup() {
-		return this.g;
+		return this.group;
 	}
 
 	public String getShownGroup() {
-		return this.sg;
+		return this.showngroup;
 	}
-
-	public void setShownGroup(final String newgroup) {
-		this.sg = newgroup;
+	
+	public String getExample() {
+		return this.format.replace("%1$s", "player").replace("%2$s", "message");
 	}
-
-	public final String getFormat() {
+	
+	public String getFormat() {
 		return this.format;
 	}
 
-	private void format() {
-		this.format = this.format
-				.replace(format.substring(0, 1) + "%g",
-						getPrefix() + format.substring(0, 1) + getShownGroup())
-				.replace("%p", getSuffix() + "%s")
-				.replace("%m", getMuffix() + "%s")
+	private void setShownGroup(String newgroup) {
+		this.showngroup = ChatColor.translateAlternateColorCodes('&', newgroup);
+	}
+
+	private void format(String mat) {
+		this.format = mat
+				.replace(mat.substring(0, 1) + "%g",
+						getPrefix() + mat.substring(0, 1) + getShownGroup())
+				.replace("%p", getSuffix() + "%1$s")
+				.replace("%m", getMuffix() + "%2$s")
 				.replace("null", ChatColor.WHITE.toString());
 	}
 
-	public String getExample() {
-		return this.format.replace("%s", "test");
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ChatProfile other = (ChatProfile) obj;
+		if (format == null) {
+			if (other.format != null)
+				return false;
+		} else if (!format.equals(other.format))
+			return false;
+		if (group == null) {
+			if (other.group != null)
+				return false;
+		} else if (!group.equals(other.group))
+			return false;
+		if (muffix == null) {
+			if (other.muffix != null)
+				return false;
+		} else if (!muffix.equals(other.muffix))
+			return false;
+		if (prefix == null) {
+			if (other.prefix != null)
+				return false;
+		} else if (!prefix.equals(other.prefix))
+			return false;
+		if (showngroup == null) {
+			if (other.showngroup != null)
+				return false;
+		} else if (!showngroup.equals(other.showngroup))
+			return false;
+		if (suffix == null) {
+			if (other.suffix != null)
+				return false;
+		} else if (!suffix.equals(other.suffix))
+			return false;
+		return true;
 	}
+
+	@Override
+	public String toString() {
+		return "ChatProfile [group=" + group + ", showngroup=" + showngroup
+				+ ", prefix=" + prefix + ", suffix=" + suffix + ", muffix="
+				+ muffix + ", format=" + ChatColor.translateAlternateColorCodes('&', format) + "]";
+	}
+
 
 }
