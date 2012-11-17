dff1a6f8b7bb3ceb5a243ad67768886985d0a1de
Nick Porillo <milkywayz@mail.com>
2012-11-17 14:11:01 -0500

    Slight change to 'fallback' format

--- src/net/milkycraft/ChatHandler.java
+++ src/net/milkycraft/ChatHandler.java
@@ -1,6 +1,5 @@
 package net.milkycraft;
 
-import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,24 +8,23 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 public class ChatHandler implements Listener {
 
 	ColoredGroups cg;
+
 	public ChatHandler(ColoredGroups cg) {
 		this.cg = cg;
 	}
 
 	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
 	public void onChat(AsyncPlayerChatEvent e) {
-		// Declare players group now, we don't want to retrieve it more than once
 		final String group = cg.getGroup(e.getPlayer());
-		for (ChatProfile c : cg.getChatProfiles()) {
+		for (final ChatProfile c : cg.getChatProfiles()) {
 			if (c.getGroup().equalsIgnoreCase(group)) {
 				e.setFormat(c.getFormat());
 				break;
 			}
 		}
 		if (e.getFormat().equals("<%1$s> %2$s")) {
-			e.setFormat(ChatColor.GRAY + "[" + group + "]" + "%s:"
-					+ ChatColor.WHITE + " %s");
+			e.setFormat("[" + group + "]" + "%1$s: %2$s");
 		}
 	}
-	
+
 }
