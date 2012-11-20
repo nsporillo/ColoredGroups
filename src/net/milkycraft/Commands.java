a56f3e760a084b2064fab8e741382ed2711a0fd4
Nick Porillo <milkywayz@mail.com>
2012-11-19 20:56:36 -0500

    Added /cg backup and /cg purge

--- src/net/milkycraft/Commands.java
+++ src/net/milkycraft/Commands.java
@@ -1,5 +1,7 @@
 package net.milkycraft;
 
+import java.util.Date;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,10 +11,10 @@ public final class Commands implements CommandExecutor {
 
 	private final String pre = ChatColor.AQUA + "[ColoredGroups] ";
 	private final String[] cmds = { "/cg help", "/cg test all",
-			"/cg test <group>", "/cg reload hooks", "/cg reload config", "/cg reload addons" };
+			"/cg test <group>", "/cg reload hooks", "/cg reload config", };
 	private final String[] helps = { "Help command for ColoredGroups",
 			"Tests all groups chat msgs", "Tests specific groups chat msgs",
-			"Rehooks into permissions", "Reloads variables from config", "Reloads addons if any" };
+			"Rehooks into permissions", "Reloads variables from config", };
 	private ColoredGroups mc;
 
 	public Commands(final ColoredGroups mc) {
@@ -31,8 +33,9 @@ public final class Commands implements CommandExecutor {
 			return true;
 		}
 		if (args[0].equalsIgnoreCase("reload")) {
-			if(args.length == 1) {
-				sender.sendMessage(ChatColor.RED + "Choose either config, hooks, or addons");
+			if (args.length == 1) {
+				sender.sendMessage(ChatColor.RED
+						+ "Choose either config, hooks, or addons");
 				return true;
 			}
 			if (args[1].equalsIgnoreCase("config")) {
@@ -54,8 +57,8 @@ public final class Commands implements CommandExecutor {
 				}
 			} else if (args[1].equalsIgnoreCase("addons")) {
 				if (sender.hasPermission("coloredgroups.reload")) {
-					mc.reloadAddons();
-					sender.sendMessage(pre + ChatColor.GREEN + "Reloaded addons");
+					sender.sendMessage(pre + ChatColor.GREEN
+							+ "Addons have been removed from ColoredGroups");
 				} else {
 					sender.sendMessage(pre + ChatColor.RED
 							+ "You dont have permission to reload");
@@ -67,6 +70,20 @@ public final class Commands implements CommandExecutor {
 				sender.sendMessage(ChatColor.YELLOW + "[" + (i + 1) + "] "
 						+ cmds[i] + ChatColor.WHITE + " - " + ChatColor.GOLD
 						+ helps[i]);
+			}
+		} else if (args[0].equalsIgnoreCase("backup")) {
+			if (sender.hasPermission("coloredgroups.backup")) {
+				this.mc.getBackupManager().create(new Date());
+				sender.sendMessage(ChatColor.GREEN + "Created a backup of the current config");
+			} else {
+				sender.sendMessage(ChatColor.RED + "You don't have permission to create backups");
+			}
+		} else if (args[0].equalsIgnoreCase("purge")) {
+			if (sender.hasPermission("coloredgroups.backup.purge")) {
+				this.mc.getBackupManager().purge();
+				sender.sendMessage(ChatColor.GREEN + "Purged all backups from the backup directory");
+			} else {
+				sender.sendMessage(ChatColor.RED + "You don't have permission to purge backups");
 			}
 		} else if (args[0].equalsIgnoreCase("test")) {
 			if (sender.hasPermission("coloredgroups.test")) {
@@ -82,9 +99,12 @@ public final class Commands implements CommandExecutor {
 					sender.sendMessage(ChatColor.RED + "Error: Argument "
 							+ exe.getLocalizedMessage() + " was empty");
 				}
+			} else {
+				sender.sendMessage(ChatColor.RED
+						+ "You don't have permission to test the colors");
 			}
 		}
 		return false;
 	}
-	
+
 }
