dee396638724db9e4fef6146cc1b10cc05763bb4
Nick Porillo <milkywayz@mail.com>
2012-11-19 20:56:51 -0500

    Added new options with built in update support

--- src/net/milkycraft/YamlConfig.java
+++ src/net/milkycraft/YamlConfig.java
@@ -4,6 +4,8 @@ import org.bukkit.configuration.ConfigurationSection;
 
 public final class YamlConfig extends YamlLoader {
 
+	public boolean debug, igs, backup;
+
 	public YamlConfig(ColoredGroups plugin, String fileName) {
 		super(plugin, fileName);
 		super.saveIfNotExist();
@@ -11,6 +13,10 @@ public final class YamlConfig extends YamlLoader {
 
 	@Override
 	protected void loadKeys() {
+		update();
+		this.debug = super.config.getBoolean("options.debug");
+		this.igs = super.config.getBoolean("options.in-game-set");
+		this.backup = super.config.getBoolean("options.backup-on-disable");
 		ConfigurationSection groups = config.getConfigurationSection("groups");
 		for (String keys : groups.getKeys(false)) {
 			ConfigurationSection vars = groups.getConfigurationSection(keys);
@@ -30,5 +36,15 @@ public final class YamlConfig extends YamlLoader {
 		super.rereadFromDisk();
 		super.load();
 		super.plugin.log("Variables reloaded from disk!");
+	}
+
+	protected void update() {
+		if (config.get("options.debug") == null) {
+			ConfigurationSection options = config.createSection("options");
+			options.set("debug", false);
+			options.set("in-game-set", true);
+			options.set("backup-on-disable", true);
+			saveConfig();
+		}
 	}
 }
