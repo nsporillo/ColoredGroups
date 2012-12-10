dd31fb2f21399a43f90b7ccfe89093f302bc5e28
Nick Porillo <milkywayz@mail.com>
2012-12-09 21:18:32 -0500

    Update help with new commands, static import

--- src/net/milkycraft/Commands.java
+++ src/net/milkycraft/Commands.java
@@ -1,110 +1,148 @@
 package net.milkycraft;
 
-import java.util.Date;
+import static org.bukkit.ChatColor.AQUA;
+import static org.bukkit.ChatColor.GOLD;
+import static org.bukkit.ChatColor.GREEN;
+import static org.bukkit.ChatColor.RED;
+import static org.bukkit.ChatColor.WHITE;
+import static org.bukkit.ChatColor.YELLOW;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public final class Commands implements CommandExecutor {
 
-	private final String pre = ChatColor.AQUA + "[ColoredGroups] ";
-	private final String[] cmds = { "/cg help", "/cg test all",
-			"/cg test <group>", "/cg reload hooks", "/cg reload config", };
+	private final String pre = AQUA + "[ColoredGroups] ";
+	private final String[] cmds = { "/cg help", "/cg test all", "/cg backup",
+			"/cg import", "/cg test <group>", "/cg reload hooks",
+			"/cg reload config", };
 	private final String[] helps = { "Help command for ColoredGroups",
-			"Tests all groups chat msgs", "Tests specific groups chat msgs",
-			"Rehooks into permissions", "Reloads variables from config", };
-	private ColoredGroups mc;
+			"Tests all groups chat msgs", "Backs up config",
+			"Imports values from prev. plugin",
+			"Tests specific groups chat msgs", "Rehooks into permissions",
+			"Reloads variables from config", };
+	private ColoredGroups cg;
 
-	public Commands(final ColoredGroups mc) {
-		this.mc = mc;
+	protected Commands(final ColoredGroups cg1) {
+		this.cg = cg1;
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
 		if (args.length == 0) {
-			sender.sendMessage(ChatColor.GREEN + "Using "
-					+ mc.getDescription().getName() + " v"
-					+ mc.getDescription().getVersion() + " by "
-					+ mc.getDescription().getAuthors().get(0));
-			sender.sendMessage(ChatColor.GREEN + "Use /cg help");
+			sender.sendMessage(GREEN + "Using " + cg.getDescription().getName()
+					+ " v" + cg.getDescription().getVersion() + " by "
+					+ cg.getDescription().getAuthors().get(0));
+			sender.sendMessage(GREEN + "Use /cg help");
 			return true;
 		}
 		if (args[0].equalsIgnoreCase("reload")) {
 			if (args.length == 1) {
-				sender.sendMessage(ChatColor.RED
-						+ "Choose either config, hooks, or addons");
+				sender.sendMessage(RED + "Choose either config or hooks");
 				return true;
 			}
 			if (args[1].equalsIgnoreCase("config")) {
 				if (sender.hasPermission("coloredgroups.reload")) {
-					mc.reload();
-					sender.sendMessage(pre + ChatColor.GREEN
-							+ "Reloaded variables");
+					cg.reload();
+					sender.sendMessage(pre + GREEN + "Reloaded variables");
 				} else {
-					sender.sendMessage(pre + ChatColor.RED
+					sender.sendMessage(pre + RED
 							+ "You dont have permission to reload");
 				}
 			} else if (args[1].equalsIgnoreCase("hooks")) {
 				if (sender.hasPermission("coloredgroups.reload")) {
-					mc.rehook();
-					sender.sendMessage(pre + ChatColor.GREEN + "Reloaded hooks");
+					cg.rehook();
+					sender.sendMessage(pre + GREEN + "Reloaded hooks");
 				} else {
-					sender.sendMessage(pre + ChatColor.RED
+					sender.sendMessage(pre + RED
 							+ "You dont have permission to reload");
 				}
 			} else if (args[1].equalsIgnoreCase("addons")) {
 				if (sender.hasPermission("coloredgroups.reload")) {
-					sender.sendMessage(pre + ChatColor.GREEN
+					sender.sendMessage(pre + GREEN
 							+ "Addons have been removed from ColoredGroups");
 				} else {
-					sender.sendMessage(pre + ChatColor.RED
+					sender.sendMessage(pre + RED
 							+ "You dont have permission to reload");
 				}
 			}
 		} else if (args[0].equalsIgnoreCase("help")) {
-			sender.sendMessage(ChatColor.GREEN + "== ColoredGroups help ==");
+			sender.sendMessage(GREEN + "== ColoredGroups help ==");
 			for (int i = 0; i < cmds.length; i++) {
-				sender.sendMessage(ChatColor.YELLOW + "[" + (i + 1) + "] "
-						+ cmds[i] + ChatColor.WHITE + " - " + ChatColor.GOLD
-						+ helps[i]);
+				sender.sendMessage(YELLOW + "[" + (i + 1) + "] " + cmds[i]
+						+ WHITE + " - " + GOLD + helps[i]);
 			}
 		} else if (args[0].equalsIgnoreCase("backup")) {
 			if (sender.hasPermission("coloredgroups.backup")) {
-				this.mc.getBackupManager().create(new Date());
-				sender.sendMessage(ChatColor.GREEN + "Created a backup of the current config");
+				this.cg.getBackupManager().create();
+				sender.sendMessage(GREEN
+						+ "Created a backup of the current config");
 			} else {
-				sender.sendMessage(ChatColor.RED + "You don't have permission to create backups");
+				sender.sendMessage(RED
+						+ "You don't have permission to create backups");
+			}
+		} else if (args[0].equalsIgnoreCase("import")) {
+			if (sender.hasPermission("coloredgroups.import")) {
+				try {
+					new ImportationWorker(cg).run(true);
+				} catch (Exception ex) {
+					sender.sendMessage(RED + "Error: " + ex.getMessage());
+					sender.sendMessage(RED
+							+ "Vault couldn't find any chat providers to import from");
+					return true;
+				}
+				sender.sendMessage(GREEN + "Imported groups via vault!");
+			} else {
+				sender.sendMessage(RED
+						+ "You don't have permission to import groups");
 			}
 		} else if (args[0].equalsIgnoreCase("purge")) {
 			if (sender.hasPermission("coloredgroups.backup.purge")) {
-				this.mc.getBackupManager().purge();
-				sender.sendMessage(ChatColor.GREEN + "Purged all backups from the backup directory");
+				this.cg.getBackupManager().purge();
+				sender.sendMessage(GREEN
+						+ "Purged all backups from the backup directory");
 			} else {
-				sender.sendMessage(ChatColor.RED + "You don't have permission to purge backups");
+				sender.sendMessage(RED
+						+ "You don't have permission to purge backups");
+			}
+		} else if (args[0].equalsIgnoreCase("create")) {
+			if (sender.hasPermission("coloredgroups.create")) {
+				try {
+					this.cg.getConfiguration().createNewGroup(args[1], "&",
+							"&", "&", "[%g]%p: %m", args[1]);
+					sender.sendMessage(GREEN
+							+ "Created an empty section in config for "
+							+ args[1]);
+				} catch (Exception ex) {
+					if (ex.getLocalizedMessage().equals("1")) {
+						sender.sendMessage(RED + "Error: /cg create <Group>");
+					}
+				}
+			} else {
+				sender.sendMessage(RED
+						+ "You don't have permission to purge backups");
 			}
 		} else if (args[0].equalsIgnoreCase("test")) {
 			if (sender.hasPermission("coloredgroups.test")) {
 				try {
 					if (args[1].equalsIgnoreCase("all")) {
-						for (ChatProfile c : mc.getChatProfiles()) {
+						for (ChatProfile c : cg.getChatProfiles()) {
 							sender.sendMessage(c.getExample());
 						}
 					} else {
-						sender.sendMessage(mc.getTestMessage(args[1]));
+						sender.sendMessage(cg.getTestMessage(args[1]));
 					}
 				} catch (Exception exe) {
-					sender.sendMessage(ChatColor.RED + "Error: Argument "
+					sender.sendMessage(RED + "Error: Argument "
 							+ exe.getLocalizedMessage() + " was empty");
 				}
 			} else {
-				sender.sendMessage(ChatColor.RED
+				sender.sendMessage(RED
 						+ "You don't have permission to test the colors");
 			}
 		}
 		return false;
 	}
-
 }
