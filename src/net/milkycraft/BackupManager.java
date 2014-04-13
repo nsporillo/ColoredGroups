ffdfaeac92c4c038fbd94ab631b6e54506cfebb4
Nick Porillo <milkywayz@mail.com>
2014-04-13 15:07:45 -0400

    No longer randomly filling up backup space
    Now config backups are made in sequential order, 0 to 50.
    
    Backups arent really too necessary though, might just remove the feature
    soon.

--- src/net/milkycraft/BackupManager.java
+++ src/net/milkycraft/BackupManager.java
@@ -5,37 +5,47 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
-import java.util.Random;
 
 public class BackupManager {
-	private File original;
+
 	private File backupdir;
 	private ColoredGroups cg;
-	private Random r = new Random();
 
 	protected BackupManager(ColoredGroups cg) {
 		this.cg = cg;
-		this.original = new File(cg.getDataFolder(), File.separator
-				+ "config.yml");
-		this.backupdir = new File(cg.getDataFolder(), File.separator
-				+ "backups" + File.separator);
+		this.backupdir = new File(cg.getDataFolder(), File.separator + "backups"
+				+ File.separator);
 	}
 
 	/**
 	 * Create a config backup
-	 * @param bounds what the Random next int uses 
+	 * 
+	 * @param limit
+	 *            max iterations to check for config
 	 */
-	public void create(int bounds) {
-		int i = r.nextInt(bounds);
+	public void create(int limit) {
 		if (!this.backupdir.exists()) {
 			this.backupdir.mkdir();
 		}
-		File backup = new File(this.backupdir, File.separator + "config_" + i
-				+ ".yml");
-		try {
-			this.copyFile(original, backup);
-		} catch (Exception e) {
-			cg.warn("Exception occured during backup process");
+		boolean completed = false;
+		for (int i = 0; i < limit; i++) {
+			File backup = new File(this.backupdir, File.separator + "config_" + i
+					+ ".yml");
+			if (!backup.exists()) {
+				try {
+					this.copyFile(new File(cg.getDataFolder(), File.separator
+							+ "config.yml"), backup);
+				} catch (Exception e) {
+					cg.warn("Exception occured during backup process");
+				}
+				completed = true;
+				cg.log("Backup " + i + " of config was saved!");
+				break;
+			}
+		}
+		if(!completed) {
+			this.purge();
+			this.create(limit);
 		}
 	}
 
@@ -49,7 +59,6 @@ public class BackupManager {
 		this.backupdir.delete();
 	}
 
-	@SuppressWarnings("resource")
 	private void copyFile(File sourceFile, File destFile) throws IOException {
 		if (!destFile.exists()) {
 			destFile.createNewFile();
@@ -59,8 +68,7 @@ public class BackupManager {
 		try {
 			source = new FileInputStream(sourceFile).getChannel();
 			destination = new FileOutputStream(destFile).getChannel();
-			cg.debug("Transfered "
-					+ destination.transferFrom(source, 0, source.size())
+			cg.debug("Transfered " + destination.transferFrom(source, 0, source.size())
 					+ " bytes of " + source.size() + " total");
 		} finally {
 			if (source != null) {
